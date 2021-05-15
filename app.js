d04999cd23ccfb12543b880880530a0f6b036185
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2021-05-16 03:02:23 +0530

    dynamic name picking logic

---
+++ app.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Page Loaded");
+    var table = document.getElementById("contributors");
+    contributors.forEach(contributor => {
+        var row = table.insertRow(-1);
+        var name = row.insertCell(0);
+        var username = row.insertCell(1);
+        var college = row.insertCell(2);
+        var year = row.insertCell(3);
+        name.innerHTML = contributor.name;
+        username.innerHTML = `<a href="https://github.com/${contributor.username}/">${contributor.username}</a>`;
+        college.innerHTML = contributor.college;
+        year.innerHTML = contributor.year;
+    })
+})
