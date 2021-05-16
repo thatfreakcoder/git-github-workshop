0820dfc85aa22e24a47c2869e5760c55db65e7a4
Chanchal Sharma <2020pietcschanchal51@poornima.org>
2021-05-16 15:59:14 +0530

    contribution of Chanchal

--- CONTRIBUTORS.js
+++ CONTRIBUTORS.js
@@ -4,10 +4,10 @@ var contributors = [{
         college: "PIET",
         year: 2
     },
-    // {
-    //     name: "YOUR NAME",
-    //     username: "YOUR GITHUB USERNAME",
-    //     college: "YOUR COLLEGE NAME",
-    //     year: 3
-    // },
+    {
+        name: "Chanchal Sharma",
+         username: "ChanchalSharma51",
+         college: "PIET",
+         year: 2
+     },
 ]
