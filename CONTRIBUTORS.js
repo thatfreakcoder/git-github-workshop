a320efafe36a682631dc4d9a61c4ea3481260350
Gourav jain <jaingaurav2301@gmail.com>
2021-05-16 15:36:12 +0530

    added contribution for gourav jain

--- CONTRIBUTORS.js
+++ CONTRIBUTORS.js
@@ -4,6 +4,12 @@ var contributors = [{
     college: "PIET",
     year: 2
 },
+ {
+     name: "GOURAV JAIN",
+     username: "gouravjain-piet",
+     college: "PIET",
+     year: 1
+ },
 // {
 //     name: "YOUR NAME",
 //     username: "YOUR GITHUB USERNAME",
