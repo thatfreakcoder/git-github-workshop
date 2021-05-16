88e3e67fdbaf29d614159255187879d0ee7941ff
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2021-05-16 15:36:04 +0530

    Added contribution for Yuvraj Dagur

--- CONTRIBUTORS.js
+++ CONTRIBUTORS.js
@@ -1,13 +1,19 @@
 var contributors = [{
-    name: "Yuvraj Dagur",
-    username: "thatfreakcoder",
-    college: "PIET",
-    year: 2
-},
-// {
-//     name: "YOUR NAME",
-//     username: "YOUR GITHUB USERNAME",
-//     college: "YOUR COLLEGE NAME",
-//     year: YOUR YEAR
-// },
+        name: "Yuvraj Dagur",
+        username: "thatfreakcoder",
+        college: "PIET",
+        year: 2
+    },
+    {
+        name: "Yuvraj Dagur",
+        username: "thatfreakcoder",
+        college: "PIET",
+        year: 2
+    },
+    // {
+    //     name: "YOUR NAME",
+    //     username: "YOUR GITHUB USERNAME",
+    //     college: "YOUR COLLEGE NAME",
+    //     year: YOUR YEAR
+    // },
 ]
