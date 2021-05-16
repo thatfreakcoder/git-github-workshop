6c0f4f8880d0e39778be8cb10aea46cf0ee1e589
Vaibhav <Vaibhavgupta0141@gmail.com>
2021-05-16 15:56:56 +0530

    added contribution for vaibhav

--- CONTRIBUTORS.js
+++ CONTRIBUTORS.js
@@ -4,10 +4,10 @@ var contributors = [{
         college: "PIET",
         year: 2
     },
-    // {
-    //     name: "YOUR NAME",
-    //     username: "YOUR GITHUB USERNAME",
-    //     college: "YOUR COLLEGE NAME",
-    //     year: 3
-    // },
+     {
+         name: "Vaibhav Gupta",
+         username: "cyber-vg",
+         college: "PIET",
+         year: 2
+     },
 ]
